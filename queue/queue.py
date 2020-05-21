c6468aedc32354de3fbfe7f82874c5a939a65234
Zac Smith <zrs3141592@gmail.com>
2020-05-21 17:27:41 -0600

    same error as stack

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,26 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from doubly_linked_list import DoublyLinkedList, ListNode
+import sys
+sys.path.append('../doubly_linked_list/doubly_linked_list.py')
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_end(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if not self.storage.head:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
