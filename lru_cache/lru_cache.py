2b0724f8f1d0a3edced1c55a64cca7bb279e61d8
Zac Smith <zrs3141592@gmail.com>
2020-05-26 19:50:02 -0600

    start LRUCache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,9 @@
+import os
+import sys
+doubly_linked_path = os.path.normpath(os.path.join(__file__, '../../doubly_linked_list'))
+sys.path.append(doubly_linked_path)
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +13,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        
+    self.max_nodes = limit
+    self.current_nodes = 0
+    self.dll = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
